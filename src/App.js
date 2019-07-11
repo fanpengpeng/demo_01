db3521279daa8a403fc644ff9be9296c106f7760
Felix F <13314209626@163.com>
2019-07-11 23:17:57 +0800

    添加热加载

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+import './App.css';
 
 class App extends Component {
+	formatName (user) {
+		return user.firstName + ' ' + user.lastName;
+	}
 	render () {
+		const name = 'React';
+		const user = {
+			firstName: 'F',
+			lastName: 'Felix'
+		};
+		const element = <h1>Hello {this.formatName(user)}</h1>;
 		return (
 			<div className="App">
-				<h1>Hello World</h1>
+				{element}
+				<h1>Hello {name}</h1>
 			</div>
-		)
+		);
 	}
 }
 
-export default App
+export default hot(module)(App);
